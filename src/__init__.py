045dc36bffad5c4ec00a42970b5652cd8119e4a7
juanpajedrez <jptriana.martinez@gmail.com>
2025-08-01 21:12:33 -0700

    All jupyter notebooks ready to explain how to downlaod -> preprocess -> obtai piano rolls -> equalizing using the src python module

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,2 @@
-from datadownloader import DataRetriever
-from utils import MidiPreprocesser, MidiToNumpy, obtain_piano_rolls, PianoRollsDiscreteEqualizer
+from .datadownloader import DataRetriever
+from .utils import MidiPreprocesser, MidiToNumpy, obtain_piano_rolls, PianoRollsDiscreteEqualizer
